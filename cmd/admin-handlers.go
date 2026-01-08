821fef4fcd058851e288fb83d0b0f0cbbfb844de
warnar boekkooi <wboekkooi@impossiblecloud.com>
2026-01-08 17:02:17 +0100

    fix nil pointer panic

--- cmd/admin-handlers.go
+++ cmd/admin-handlers.go
@@ -2936,7 +2936,9 @@ func (a adminAPIHandlers) ListObjectsHandler(w http.ResponseWriter, r *http.Requ
 		errsStr := errors.Join(errs...).Error()
 		logger.Error(fmt.Sprintf("Failures occurred while walking %s:\n%s", bucketName, errsStr))
 		_ = target.Write([]string{errsStr})
-		_ = target.Write([]string{"parent context: " + ctx.Err().Error()})
+		if ctx.Err() != nil {
+			_ = target.Write([]string{"parent context: " + ctx.Err().Error()})
+		}
 	}
 }
 
